6b9c3f4c2d2d5c3a91112b503a9d63047bd61683
Ornella Meneghini <meneghini.ornella@gmail.com>
2023-03-04 15:00:40 -0300

    update links

---
+++ app.js
@@ -0,0 +1,44 @@
+const proyects = [
+    [
+        {
+            "title": "Portfolio",
+            "img": "portfolio.png",
+            "description": "Portfolio created in Vue.",
+            "urlGithub": "https://github.com/MeneghiniOrnella/MeneghiniOrnella.github.io",
+            "urlSite":"https://meneghiniornella.netlify.app/",
+            "skills": ["vue", "javascript", "html", "css"]
+        },
+        {
+            "title": "Pokemon Game",
+            "img": "pokeapi.png",
+            "description": "Guess the pokemon game.It was made using Pokeapi and Vue.",
+            "urlGithub": "https://github.com/MeneghiniOrnella/pokemon-game",
+            "urlSite":"",
+            "skills": ["api", "vue", "javascript", "html", "css"]
+        },
+        {
+            "title": "Mascotas",
+            "img": "dog.jpg",
+            "description": "Team proyect of Codo a Codo Python 2021",
+            "urlGithub": "https://github.com/MeneghiniOrnella/mascotas",
+            "urlSite":"",
+            "skills": ["python", "flask", "mysql", "bootstrap", "html", "css"]
+        },
+        {
+            "title": "Gamedo",
+            "img": "gamedo.png",
+            "description": "Multiple choice game made for a taekwondo school.",
+            "urlGithub": "https://github.com/MeneghiniOrnella/tkd-gamedo",
+            "urlSite":"https://gamedo.netlify.app/",
+            "skills": ["html", "javascript", "css"]
+        },
+        {
+            "title": "Role Game",
+            "img": "logo.png",
+            "description": "Sorry! This application is being created.",
+            "urlGithub": "https://github.com/MeneghiniOrnella/vue-role-game.git",
+            "urlSite":"",
+            "skills": ["vue", "javascript", "html", "css"]
+        }
+    ]
+];
