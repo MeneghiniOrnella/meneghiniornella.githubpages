e52029ab6e143b3341ccfa0e4ce0a2c6c93873f5
Ornella Meneghini <meneghini.ornella@gmail.com>
2023-03-04 17:18:07 -0300

    update js projects

--- app.js
+++ app.js
@@ -1,50 +1,67 @@
+const project = document.getElementById("project");
+
 const data = [
-    [
-        {
-            "title": "Portfolio",
-            "img": "portfolio.png",
-            "description": "Portfolio created in Vue.js.",
-            "urlGithub": "https://github.com/MeneghiniOrnella/MeneghiniOrnella.github.io",
-            "urlSite":"https://meneghiniornella.netlify.app/",
-            "skills": ["Vue.js", "Javascript", "HTML", "CSS"]
-        },
-        {
-            "title": "Pokemon Game",
-            "img": "pokeAPI.png",
-            "description": "Guess the pokemon game.It was made using PokeAPI and Vue.js.",
-            "urlGithub": "https://github.com/MeneghiniOrnella/pokemon-game",
-            "urlSite":"",
-            "skills": ["API", "Vue.js", "Javascript", "HTML", "CSS"]
-        },
-        {
-            "title": "Mascotas",
-            "img": "dog.jpg",
-            "description": "Team proyect of Codo a Codo Python 2021",
-            "urlGithub": "https://github.com/MeneghiniOrnella/mascotas",
-            "urlSite":"",
-            "skills": ["Python", "Flask", "MySQL", "Bootstrap", "HTML", "CSS"]
-        },
-        {
-            "title": "Gamedo",
-            "img": "gamedo.png",
-            "description": "Multiple choice game made for a taekwondo school.",
-            "urlGithub": "https://github.com/MeneghiniOrnella/tkd-gamedo",
-            "urlSite":"https://gamedo.netlify.app/",
-            "skills": ["HTML", "Javascript", "CSS"]
-        },
-        {
-            "title": "Role Game",
-            "img": "logo.png",
-            "description": "Sorry! This application is being created.",
-            "urlGithub": "https://github.com/MeneghiniOrnella/Vue.js-role-game.git",
-            "urlSite":"",
-            "skills": ["Vue.js", "Javascript", "HTML", "CSS"]
-        }
-    ]
+    {
+        "title": "Portfolio",
+        "img": "portfolio.png",
+        "description": "Portfolio created in Vue.js.",
+        "urlGithub": "https://github.com/MeneghiniOrnella/MeneghiniOrnella.github.io",
+        "urlSite":"https://meneghiniornella.netlify.app/",
+        "skills": ["Vue.js", "Javascript", "HTML", "CSS"]
+    },
+    {
+        "title": "Pokemon Game",
+        "img": "pokeAPI.png",
+        "description": "Guess the pokemon game.It was made using PokeAPI and Vue.js.",
+        "urlGithub": "https://github.com/MeneghiniOrnella/pokemon-game",
+        "urlSite":"",
+        "skills": ["API", "Vue.js", "Javascript", "HTML", "CSS"]
+    },
+    {
+        "title": "Mascotas",
+        "img": "dog.jpg",
+        "description": "Team proyect of Codo a Codo Python 2021",
+        "urlGithub": "https://github.com/MeneghiniOrnella/mascotas",
+        "urlSite":"",
+        "skills": ["Python", "Flask", "MySQL", "Bootstrap", "HTML", "CSS"]
+    },
+    {
+        "title": "Gamedo",
+        "img": "gamedo.png",
+        "description": "Multiple choice game made for a taekwondo school.",
+        "urlGithub": "https://github.com/MeneghiniOrnella/tkd-gamedo",
+        "urlSite":"https://gamedo.netlify.app/",
+        "skills": ["HTML", "Javascript", "CSS"]
+    },
+    {
+        "title": "Role Game",
+        "img": "logo.png",
+        "description": "Sorry! This application is being created.",
+        "urlGithub": "https://github.com/MeneghiniOrnella/Vue.js-role-game.git",
+        "urlSite":"",
+        "skills": ["Vue.js", "Javascript", "HTML", "CSS"]
+    }
 ];
 
-const projects() {
-    return data.map((project) => {
-      return project;
-    })
-}
+for(let i = 0; i < data.length; i++) {
+    let card = document.createElement("div");
+    let cardImg = document.createElement("img");
+    let cardTitle = document.createElement("h4");
+    let cardDescription = document.createElement("p");
+    let cardUrlGithub = document.createElement("a");
+    let cardSkills = document.createElement("div");
+
+    cardImg.innerHTML = data[i].img;
+    cardTitle.innerHTML = data[i].title;
+    cardDescription.innerHTML = data[i].description;
+    cardUrlGithub.innerHTML = data[i].urlGithub;
+    cardSkills.innerHTML = data[i].skills;
+
+    card.appendChild(cardImg);
+    card.appendChild(cardTitle);
+    card.appendChild(cardDescription);
+    card.appendChild(cardUrlGithub);
+    card.appendChild(cardSkills);
+    
+    project.appendChild(card);
+};
